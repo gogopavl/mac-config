68eb311da882422542e2412ed5b240ae6e8bd8ca
gogopavl <pgogousis@gmail.com>
2022-07-30 16:30:43 -0600

    add software installer script

---
+++ install-software.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+### <OS Utilities>
+
+# Reshape, resize and reorder your windows with simple keystrokes
+brew install --cask spectacle
+
+# Your go-to search bar
+brew install --cask alfred
+
+# GIF recorder, awesome for capturing short demos & how-tos
+brew install --cask licecap
+
+# Copy-paste cache
+brew install --cask maccy
+
+### </OS Utilities>
+
+### <Development>
+
+# JetBrains app to manage IDEs
+brew install --cask jetbrains-toolbox
+
+# Default text editor
+brew install --cask visual-studio-code
+
+# Container management
+brew install --cask docker
+
+# RDBMS client
+brew install --cask dbeaver-community
+
+# Redis cache client
+brew install --cask redisinsight
+
+# Kafka topic client
+brew install --cask offset-explorer
+
+# Default terminal
+brew install --cask iterm2
+
+# K8s deployment management
+brew install --cask lens
+
+# HTTP request client
+brew install --cask postman
+
+### </Development>
